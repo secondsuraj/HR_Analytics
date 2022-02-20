981dd1d5da10f765f7d403d11b157820366439d7
Suraj <pandeysuraj3110@gmail.com>
2022-02-20 13:29:35 +0530

    first commit

---
+++ explore_page.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+def show_explore_page():
+    st.title("Explore HR Analytics Problem")
+
+    st.write(
+        """
+    ### For more, check the [Analytics Vidhya Datahack Platform](https://datahack.analyticsvidhya.com/contest/wns-analytics-hackathon-2018-1/#ProblemStatement)
+    """
+    )
+    st.write("")
